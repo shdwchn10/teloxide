6143952f71a3dbb6cacad844825b8a8feddf0887
Andrey Brusnik <dev@shdwchn.io>
2024-07-19 18:22:08 +0400

    Add `getUserChatBoosts` TBA method

--- crates/teloxide-core/src/adaptors/erased.rs
+++ crates/teloxide-core/src/adaptors/erased.rs
@@ -259,6 +259,7 @@ where
         unhide_general_forum_topic,
         unpin_all_general_forum_topic_messages,
         answer_callback_query,
+        get_user_chat_boosts,
         set_my_commands,
         get_my_commands,
         set_my_name,
@@ -736,6 +737,12 @@ trait ErasableRequester<'a> {
         &self,
         callback_query_id: String,
     ) -> ErasedRequest<'a, AnswerCallbackQuery, Self::Err>;
+
+    fn get_user_chat_boosts(
+        &self,
+        chat_id: Recipient,
+        user_id: UserId,
+    ) -> ErasedRequest<'a, GetUserChatBoosts, Self::Err>;
 
     fn set_my_commands(
         &self,
@@ -1577,6 +1584,14 @@ where
         callback_query_id: String,
     ) -> ErasedRequest<'a, AnswerCallbackQuery, Self::Err> {
         Requester::answer_callback_query(self, callback_query_id).erase()
+    }
+
+    fn get_user_chat_boosts(
+        &self,
+        chat_id: Recipient,
+        user_id: UserId,
+    ) -> ErasedRequest<'a, GetUserChatBoosts, Self::Err> {
+        Requester::get_user_chat_boosts(self, chat_id, user_id).erase()
     }
 
     fn set_my_commands(
