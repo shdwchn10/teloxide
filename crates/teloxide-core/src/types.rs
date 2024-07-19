016737ea819c240fcc056d41403b6ba9cea83b0d
Andrey Brusnik <dev@shdwchn.io>
2024-07-19 14:20:40 +0400

    Move deserialization prefix for `giveaway_message_id` field to types module
    because it will be used in `ChatBoostSourceGiveaway` later

--- crates/teloxide-core/src/types.rs
+++ crates/teloxide-core/src/types.rs
@@ -297,6 +297,10 @@ pub use seconds::*;
 pub use user_id::*;
 
 use serde::Serialize;
+use serde_with::with_prefix;
+
+// Deserialization prefix for giveaway_message_id field used in GiveawayWinners
+with_prefix!(prefix_giveaway_message_id "giveaway_");
 
 /// Converts an `i64` timestamp to a `choro::DateTime`, producing serde error
 /// for invalid timestamps
